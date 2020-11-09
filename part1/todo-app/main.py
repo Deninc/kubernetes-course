9e05a3fa8af0bd265528c42e6af39476246ada36
Thang <thang.nguyen@cloudasset.com>
2020-11-09 22:58:54 +0200

    1.13

--- part1/todo-app/main.py
+++ part1/todo-app/main.py
@@ -1,15 +1,23 @@
 import urllib.request
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 # http://localhost/public/image.jpg
-app.mount("/public", StaticFiles(directory="public"), name="public")
-urllib.request.urlretrieve("https://picsum.photos/400/300", "/app/public/image.jpg")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+urllib.request.urlretrieve("https://picsum.photos/400/300", "/app/static/image.jpg")
+
+templates = Jinja2Templates(directory="templates")
+
 
 # http://localhost/
 @app.get("/")
-def read_root():
-    return FileResponse('public/index.html')
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"todo": "nothing", "request": request})
+
+@app.post("/add")
+def add(request: Request, todo: str = Form(...)):
+    return templates.TemplateResponse("index.html", {"todo": todo, "request": request})
