2e213987a1a92608ab4d6ca3197fe1644711056d
Thang <thang.nguyen@cloudasset.com>
2020-11-15 11:05:04 +0200

    2.02 project v1.0

--- part2/todo-app/README.md
+++ part2/todo-app/README.md
@@ -1,17 +1,9 @@
 This project include exercise:
-- 1.02
-- 1.04
-- 1.05
-- 1.06
-- 1.08
-- 1.12
+- 2.02
 
 ```bash
-docker build -t thangnv2212/todo-app .
-docker push thangnv2212/todo-app
+docker build -t thangnv2212/todo-app . && docker push thangnv2212/todo-app
 kubectl apply -f manifests/deployment.yaml
 kubectl apply -f manifests/service.yaml
 kubectl apply -f manifests/ingress.yaml
 ```
-
-Deployed to localhost:8081, delete main-app ingress before running this.
--- part2/todo-app/main.py
+++ part2/todo-app/main.py
@@ -1,12 +1,12 @@
 import urllib.request
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# http://localhost/public/image.jpg
+# http://localhost/static/image.jpg
 app.mount("/static", StaticFiles(directory="static"), name="static")
 urllib.request.urlretrieve("https://picsum.photos/400/300", "/app/static/image.jpg")
 
@@ -14,10 +14,20 @@ templates = Jinja2Templates(directory="templates")
 
 
 # http://localhost/
-@app.get("/")
+@app.get("/todos")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"todo": "nothing", "request": request})
+    try:
+        with open("/tmp/list.txt") as f:
+                todos = f.read().splitlines()
+    except FileNotFoundError:
+        todos = []
 
-@app.post("/add")
-def add(request: Request, todo: str = Form(...)):
-    return templates.TemplateResponse("index.html", {"todo": todo, "request": request})
+    return templates.TemplateResponse("index.html", {"todos": todos, "request": request})
+
+@app.post("/todos")
+def add(todo: str = Form(...)):
+    ### ... means todo param is required
+    with open("/tmp/list.txt", "a") as f:
+        f.write(todo + "\n")
+
+    return RedirectResponse("/todos", status_code=status.HTTP_303_SEE_OTHER)
