033cc62fbe2741c120f09911e333faf14e3ea6d4
Thang <thangnv2212@gmail.com>
2020-11-18 23:26:54 +0200

    fix: db trx not committed

--- part2/pong-app/main.py
+++ part2/pong-app/main.py
@@ -5,21 +5,27 @@ from fastapi import FastAPI
 app = FastAPI()
 
 conn = psycopg2.connect(host="pg-svc", dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
-cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS counter (id serial PRIMARY KEY, count integer);")
-cur.execute("INSERT INTO counter(id, count) VALUES (1, 0) ON CONFLICT (id) DO NOTHING;")
+# https://www.psycopg.org/docs/usage.html#with-statement
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS counter (id serial PRIMARY KEY, count integer);")
+        cur.execute("INSERT INTO counter(id, count) VALUES (1, 0) ON CONFLICT (id) DO NOTHING;")
 
 @app.get("/")
 def read_root():
-    cur.execute("SELECT count FROM counter WHERE id = 1")
-    c = cur.fetchone()[0]
-    c += 1
-    cur.execute(f"UPDATE counter SET count = {c} WHERE id = 1")
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT count FROM counter WHERE id = 1")
+            c = cur.fetchone()[0]
+            c += 1
+            cur.execute(f"UPDATE counter SET count = {c} WHERE id = 1")
     return f"pong {c}"
 
 @app.get("/count")
 def read_count():
-    cur.execute("SELECT count FROM counter WHERE id = 1")
-    c = cur.fetchone()[0]
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT count FROM counter WHERE id = 1")
+            c = cur.fetchone()[0]
     return c
--- part2/todo-app/main.py
+++ part2/todo-app/main.py
@@ -15,21 +15,26 @@ urllib.request.urlretrieve("https://picsum.photos/400/300", "/app/static/image.j
 templates = Jinja2Templates(directory="templates")
 
 conn = psycopg2.connect(host="pg-svc", dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
-cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar);")
+# https://www.psycopg.org/docs/usage.html#with-statement
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar);")
 
-# http://localhost/
 @app.get("/todos")
 def read_root(request: Request):
-    cur.execute("SELECT todo FROM todos")
-    todos = [x[0] for x in cur.fetchall()]
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT todo FROM todos")
+            todos = [x[0] for x in cur.fetchall()]
 
     return templates.TemplateResponse("index.html", {"todos": todos, "request": request})
 
 @app.post("/todos")
 def add(todo: str = Form(...)):
     ### ... means todo param is required
-    cur.execute(f"INSERT INTO todos(todo) VALUES ('{todo}')")
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(f"INSERT INTO todos(todo) VALUES ('{todo}')")
 
     return RedirectResponse("/todos", status_code=status.HTTP_303_SEE_OTHER)
