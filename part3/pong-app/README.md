2239315ba9d8f7739fa420853e192f630e50cd91
Thang <thang.nguyen@cloudasset.com>
2020-11-22 20:48:31 +0200

    copy from part2

---
+++ part3/pong-app/README.md
@@ -0,0 +1,9 @@
+```bash
+docker build -t thangnv2212/pong-app . && docker push thangnv2212/pong-app
+
+kubectl apply -f manifests/secret.yaml
+kubectl apply -f manifests/deployment.yaml
+kubectl apply -f manifests/statefulset.yaml
+```
+
+Go to localhost:8081/pingpong
---
+++ part3/pong-app/main.py
@@ -0,0 +1,31 @@
+import os
+import psycopg2
+from fastapi import FastAPI
+
+app = FastAPI()
+
+conn = psycopg2.connect(host="pg-svc", dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
+
+# https://www.psycopg.org/docs/usage.html#with-statement
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS counter (id serial PRIMARY KEY, count integer);")
+        cur.execute("INSERT INTO counter(id, count) VALUES (1, 0) ON CONFLICT (id) DO NOTHING;")
+
+@app.get("/")
+def read_root():
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT count FROM counter WHERE id = 1")
+            c = cur.fetchone()[0]
+            c += 1
+            cur.execute(f"UPDATE counter SET count = {c} WHERE id = 1")
+    return f"pong {c}"
+
+@app.get("/count")
+def read_count():
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT count FROM counter WHERE id = 1")
+            c = cur.fetchone()[0]
+    return c
