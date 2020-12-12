0bc5897b56ca2887912255657d77a5d2e06f4cf8
Thang <thang.nguyen@cloudasset.com>
2020-12-12 18:21:51 +0200

    4.02

--- part3/todo-app/main.py
+++ part3/todo-app/main.py
@@ -2,7 +2,7 @@ import os
 import logging
 import psycopg2
 import urllib.request
-from fastapi import FastAPI, Form, Request, status
+from fastapi import FastAPI, Form, Request, status, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -20,12 +20,8 @@ urllib.request.urlretrieve("https://picsum.photos/400/300", "/app/static/image.j
 
 templates = Jinja2Templates(directory="templates")
 
-conn = psycopg2.connect(host="pg-svc", dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
+conn = None
 
-# https://www.psycopg.org/docs/usage.html#with-statement
-with conn:
-    with conn.cursor() as cur:
-        cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar);")
 
 # Fix GKE Ingress 502 error
 @app.get("/")
@@ -53,3 +49,21 @@ def add(todo: str = Form(...)):
         logger.warn(f"Todo message too long, not added")
 
     return RedirectResponse("/todos", status_code=status.HTTP_303_SEE_OTHER)
+
+# readinessProbe check if Postgres is working
+@app.get("/rediness")
+def rediness():
+    global conn
+    try:
+        if not conn:
+            conn = psycopg2.connect(host="pg-svc", dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
+
+        # https://www.psycopg.org/docs/usage.html#with-statement
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar);")
+    except Exception as e:
+        print(e)
+        conn = None
+        raise HTTPException(status_code=500, detail="Postgres server not ready")
+    return
