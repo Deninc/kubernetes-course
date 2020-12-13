847bc4689c1329abacb3ccc38a539a5080bc68bf
Thang <thang.nguyen@cloudasset.com>
2020-12-13 16:13:26 +0200

    4.05

--- part3/todo-app/main.py
+++ part3/todo-app/main.py
@@ -30,12 +30,15 @@ def health():
 
 @app.get("/todos")
 def read_root(request: Request):
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT todo FROM todos")
-            todos = [x[0] for x in cur.fetchall()]
-
-    return templates.TemplateResponse("index.html", {"todos": todos, "request": request})
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT todo, done FROM todos")
+                todos = [x for x in cur.fetchall()]
+        return templates.TemplateResponse("index.html", {"todos": todos, "request": request})
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Server error")
 
 @app.post("/todos")
 def add(todo: str = Form(...)):
@@ -50,6 +53,17 @@ def add(todo: str = Form(...)):
 
     return RedirectResponse("/todos", status_code=status.HTTP_303_SEE_OTHER)
 
+@app.put("/todos/{todo_id}")
+def add(todo_id: int):
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f"UPDATE todos SET done='t' WHERE id = {todo_id}")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Server error")
+    return RedirectResponse("/todos", status_code=status.HTTP_303_SEE_OTHER)
+
 # readinessProbe check if Postgres is working
 @app.get("/rediness")
 def rediness():
@@ -61,7 +75,7 @@ def rediness():
         # https://www.psycopg.org/docs/usage.html#with-statement
         with conn:
             with conn.cursor() as cur:
-                cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar);")
+                cur.execute("CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, todo varchar, done boolean default 'f');")
     except Exception as e:
         print(e)
         conn = None
